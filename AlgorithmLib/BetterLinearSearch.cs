df9980b5b049fb9e6ad9e4cfcf2d4f4bb077552b
Alex Perazzo <theodoreperazzo@byui.edu>
2023-09-15 12:22:32 -0600

    Finished Project

--- AlgorithmLib/BetterLinearSearch.cs
+++ AlgorithmLib/BetterLinearSearch.cs
@@ -4,7 +4,23 @@ public static class BetterLinearSearch
 {
     public static int Search(List<IComparable> data, IComparable target)
     {
-        // ADD CODE HERE AND FIX RETURN STATEMENT
+        var index = 0;
+        //set initial index to check
+        
+        foreach (var i in data)
+            //cycle through each thing in the list
+        {
+            
+            if (i.Equals(target))
+                //check if it's equal to the target
+            {
+                return index;
+                //if it is equal, return the index of the target
+            }
+
+            index++;
+        }
+        
         return -1;
     }
 }
